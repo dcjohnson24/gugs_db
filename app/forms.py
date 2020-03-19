fd4f35270f7a4e40f7b96a8f85151c31b142a704
dylan <dcjohnson24@gmail.com>
2020-03-18 22:11:52 -0500

    Add top 10 endpoint

--- app/forms.py
+++ app/forms.py
@@ -1,13 +1,20 @@
-from wtforms import Form, StringField, SelectField
+from wtforms import Form, StringField, SelectField, IntegerField
 
 
 class RunnerSearchForm(Form):
     choices = [('Runner Name', 'Runner Name')]
     select = SelectField('Search for GUGS runners:', choices=choices)
-    search = StringField('')
+    search = StringField('Search by runner name')
 
 
 class RunnerRaceSearchForm(Form):
     choices = [('Runner Name', 'Runner Name')]
     select = SelectField('Find races for a GUGS runner', choices=choices)
-    search = StringField('')
+    search = StringField('Search by runner name')
+
+
+class TopRunnerForm(Form):
+    choices = [('male', 'Male'), ('female', 'Female')]
+    select = SelectField('Find top runners by gender', choices=choices)
+    search = StringField('Enter race name')
+    n = IntegerField('Number of runners to display (integer)')
